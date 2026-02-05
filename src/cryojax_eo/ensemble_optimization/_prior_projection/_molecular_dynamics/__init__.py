8088c9f3bada8f168f2059ba43bb380bbf8eda6c
David Silva-Sánchez <31875788+DSilva27@users.noreply.github.com>
2026-02-05 16:12:31 -0500

    Revert "Change steering force `0.5 k RMSD^2` to `k RMSD` and automate computation of the steering constant"

--- src/cryojax_eo/ensemble_optimization/_prior_projection/_molecular_dynamics/__init__.py
+++ src/cryojax_eo/ensemble_optimization/_prior_projection/_molecular_dynamics/__init__.py
@@ -1,5 +1,4 @@
 from .openmm import (
     EnsembleSteeredMDSimulator as EnsembleSteeredMDSimulator,
     SteeredMDSimulator as SteeredMDSimulator,
-    compute_biasing_constant as compute_biasing_constant,
 )
